daae4e0056a90b49446636efc405d5c45c13b70a
Elizia <eliziajs@gmail.com>
2022-08-05 09:44:18 -0300

    criação de tela de cadastro

---
+++ app/src/main/java/com/elizia/av1_1821312095/cadastro.kt
@@ -0,0 +1,11 @@
+package com.elizia.av1_1821312095
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class cadastro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro)
+    }
+}
